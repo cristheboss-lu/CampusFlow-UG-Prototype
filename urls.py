f5890cd7c3304e14df1ab47c4b115fc86b244f1f
cristheboss-lu <angiereyes130801@gmail.com>
2026-08-23 22:27:17 -0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import login_view, logout_view, cursos_view
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -9,10 +10,6 @@ urlpatterns = [
     path('admisiones/', views.admisiones, name='admisiones'),
     path('contacto/', views.contacto, name='contacto'),
     path('contacto-exito/', views.contacto_exito, name='contacto_exito'),
-]
-from .views import login_view, logout_view, cursos_view
-
-urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('mis-cursos/', cursos_view, name='cursos'),
