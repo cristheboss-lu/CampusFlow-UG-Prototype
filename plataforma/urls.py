70569316990ccfbfa28144b7ca693a0d623a0bf3
cristheboss-lu <angiereyes130801@gmail.com>
2026-08-24 23:35:17 -0500

    Update urls.py

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -1,20 +1,14 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
-    # --- Públicas ---
     path('', views.index, name='index'),
     path('portal-estudiantil/', views.portal_estudiantil, name='portal_estudiantil'),
     path('biblioteca/', views.biblioteca, name='biblioteca'),
     path('admisiones/', views.admisiones, name='admisiones'),
     path('contacto/', views.contacto, name='contacto'),
     path('contacto-exito/', views.contacto_exito, name='contacto_exito'),
-
-    # --- Auth ---
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-
-    # --- Área Virtual (protegida) ---
     path('aulas-virtuales/', views.aulas_virtuales, name='aulas_virtuales'),
     path('mis-cursos/', views.cursos_view, name='cursos'),
 ]
